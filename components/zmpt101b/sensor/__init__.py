35a1a6b6bbd5f058ce7ee9d10f8f39e9f03fdcdd
idreamshen <idream.shen@gmail.com>
2026-01-10 15:13:32 +0000

    feat: Add ZMPT101B sensor support with improved config

--- components/zmpt101b/sensor/__init__.py
+++ components/zmpt101b/sensor/__init__.py
@@ -1,19 +1,57 @@
 import esphome.codegen as cg
+from esphome.components import sensor, voltage_sampler
 import esphome.config_validation as cv
-from esphome.components import sensor
-from esphome.const import CONF_ID
+from esphome.const import (
+    CONF_ID,
+    CONF_SENSOR,
+    DEVICE_CLASS_VOLTAGE,
+    ICON_FLASH,
+    STATE_CLASS_MEASUREMENT,
+    UNIT_VOLT,
+)
 
-sensor_ns = cg.esphome_ns.namespace('zmpt101b_ns')
+CODEOWNERS = ["@hzkincony"]
+DEPENDENCIES = []
+AUTO_LOAD = ["voltage_sampler"]
 
-Zmpt101bSensor = sensor_ns.class_('ZMPT101BSensor', cg.PollingComponent)
+zmpt101b_ns = cg.esphome_ns.namespace("zmpt101b")
 
-CONFIG_SCHEMA = sensor.sensor_schema(Zmpt101bSensor).extend({
-    cv.GenerateID(): cv.declare_id(Zmpt101bSensor),
-    cv.Required("adc_pin"): cv.int_range(min=0, max=99),
-    cv.Optional("sensitivity", default=500): cv.float_range(min=0, max=1000)
-})
+ZMPT101BSensor = zmpt101b_ns.class_(
+    "ZMPT101BSensor",
+    sensor.Sensor,
+    cg.PollingComponent,
+)
 
-def to_code(config):
-    var = cg.new_Pvariable(config[CONF_ID], config["adc_pin"], config["sensitivity"])
-    yield cg.register_component(var, config)
-    yield sensor.register_sensor(var, config)
+CONF_SENSITIVITY = "sensitivity"
+CONF_FREQUENCY = "frequency"
+
+CONFIG_SCHEMA = (
+    sensor.sensor_schema(
+        ZMPT101BSensor,
+        unit_of_measurement=UNIT_VOLT,
+        accuracy_decimals=2,
+        device_class=DEVICE_CLASS_VOLTAGE,
+        state_class=STATE_CLASS_MEASUREMENT,
+        icon=ICON_FLASH,
+    )
+    .extend(
+        {
+            cv.Required(CONF_SENSOR): cv.use_id(voltage_sampler.VoltageSampler),
+            cv.Optional(CONF_SENSITIVITY, default=500.0): cv.float_range(min=1.0, max=10000.0),
+            cv.Optional(CONF_FREQUENCY, default=50): cv.int_range(min=50, max=60),
+        }
+    )
+    .extend(cv.polling_component_schema("60s"))
+)
+
+
+async def to_code(config):
+    var = cg.new_Pvariable(config[CONF_ID])
+    await sensor.register_sensor(var, config)
+    await cg.register_component(var, config)
+
+    cg.add(var.set_sensitivity(config[CONF_SENSITIVITY]))
+    cg.add(var.set_frequency(config[CONF_FREQUENCY]))
+
+    sens = await cg.get_variable(config[CONF_SENSOR])
+    cg.add(var.set_source(sens))
